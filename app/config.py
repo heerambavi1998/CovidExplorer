a81ad931d6818bfa80588c5cd9204656c34a126e
Kavita Vaishnaw <54227800+kavita-v@users.noreply.github.com>
2020-04-15 17:59:56 +0530

    update indexing func and config paths

--- app/config.py
+++ app/config.py
@@ -1,9 +1,13 @@
 
 ES_PORT = 9200
-DATAPATHS = ["data/comm_use_subset/comm_use_subset/*.json",
-            "data/biorxiv_medrxiv/biorxiv_medrxiv/*.json",
-            "data/custom_license/custom_license/*.json",
-            "data/noncomm_use_subset/noncomm_use_subset/*.json",
+#place all pdf_json files above pmc_json files since entity csv is constructed with preference to pdf sha over pmc id
+DATAPATHS = ["data/comm_use_subset/comm_use_subset/pdf_json/*.json",
+            "data/noncomm_use_subset/noncomm_use_subset/pdf_json/*.json",
+            "data/biorxiv_medrxiv/biorxiv_medrxiv/pdf_json/*.json",
+            "data/custom_license/custom_license/pdf_json/*.json",
+            "data/custom_license/custom_license/pmc_json/*.json",            
+            "data/noncomm_use_subset/noncomm_use_subset/pmc_json/*.json",
+            "data/comm_use_subset/comm_use_subset/pmc_json/*.json",
             ]
 METADATAPATH = "data/metadata.csv"
 NERDATAPATH = "prge_from_abs_comb.csv"
